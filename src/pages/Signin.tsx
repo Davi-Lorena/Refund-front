31c6650949de1504e4e09b4a9d7ee01d2063fa23
Davi Lorena <davilorena2016@gmail.com>
2026-02-04 14:39:20 -0300

    feat: cria componente de input

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,3 +1,11 @@
+import { Input } from "../components/Input";
+
 export function SignIn() {
-  return <h1>Signin Page</h1>;
+  return (
+    <form className="flex flex-col gap-4">
+<Input required legend="E-mail" type="email" placeholder="seu@email"/>
+
+<Input required legend="Senha" type="password" placeholder="123456"/>
+    </form>
+  )
 }

881eaf84a1c3e63927be6285a6c9302625388b5c
Davi Lorena <davilorena2016@gmail.com>
2026-02-04 15:04:55 -0300

    feat: adiciona pagina de signup

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router"
 import { AuthLayout } from "../components/AuthLayout"
 
 import { SignIn } from "../pages/Signin"
+import { SignUp } from "../pages/Signup"
 
 export function AuthRoutes() {
     return (
 <Routes>
     <Route path="/" element={<AuthLayout />}>
         <Route path="/" index element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
     </Route>
 </Routes>
     )

933ad5a34842994944a93a4b4520dd8f799de1e1
Davi Lorena <davilorena2016@gmail.com>
2026-02-05 13:49:15 -0300

    feat: adiciona layout da aplicacao

--- src/routes/EmplyoeeRoutes.tsx
+++ src/routes/EmplyoeeRoutes.tsx
@@ -2,12 +2,13 @@ import { Routes, Route } from "react-router"
 
 import { Refund } from "../pages/Refund"
 import { NotFound } from "../pages/NotFound"
+import { AppLayout } from "../components/AppLayout"
 
 
 export function EmployeeRoutes() {
 return (
 <Routes>
-    <Route> 
+    <Route path="/" element={<AppLayout />}> 
         <Route path="/" element={<Refund />} />
     </Route>
 

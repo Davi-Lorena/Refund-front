5d95c777c6097875b0ee1f283b59d2b2c7ffdfc4
Davi Lorena <davilorena2016@gmail.com>
2026-02-05 15:14:24 -0300

    feat: adiciona pagina de confirmacao de envio de solicitacao

--- src/routes/EmplyoeeRoutes.tsx
+++ src/routes/EmplyoeeRoutes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router"
 import { Refund } from "../pages/Refund"
 import { NotFound } from "../pages/NotFound"
 import { AppLayout } from "../components/AppLayout"
+import { Confirm } from "../pages/Confirm"
 
 
 export function EmployeeRoutes() {
@@ -10,6 +11,7 @@ return (
 <Routes>
     <Route path="/" element={<AppLayout />}> 
         <Route path="/" element={<Refund />} />
+        <Route path="/confirm" element={<Confirm />} />
     </Route>
 
     <Route path="*" element={<NotFound />} />

c3cfa3902548f08533378198b28d94d10179113c
Davi Lorena <davilorena2016@gmail.com>
2026-02-09 14:34:14 -0300

    feat: adiciona merge de classes tailwindcss

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,24 @@
+import { ClassMerge } from "../utils/ClassMerge"
+
 type Props = React.ComponentProps<"button"> & {
     isLoading?: boolean
+    variant?: "base" | "icon"
 }
 
-export function Button({ children, isLoading, type = "button", ...rest }: Props) {
+const variants = {
+    button: {
+        base: "h-12",
+        icon: "h-12 w-12"
+    },
+}
+
+export function Button({ children, className, isLoading, type = "button", variant = "base", ...rest }: Props) {
     return (
-        <button type={type} disabled={isLoading} {...rest} className="flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-progress h-12">
+        <button type={type} disabled={isLoading} {...rest} className={
+            ClassMerge(["flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-progress",
+                variants.button[variant], className
+            ])
+            }>
 {children}
         </button>
     )
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
+import searchSvg from "../assets/search.svg"
 import { Input } from "../components/Input"
+import { Button } from "../components/Button"
+
 
 export function Dashboard() {
 const [name, setName] = useState("")
@@ -17,6 +20,10 @@ e.preventDefault()
 
 <form onSubmit={fetchRefunds} className="flex flex-1 items-center justify-between pb-6 border-b border-b-gray-400 md:flex-row gap-2 mt-6">
     <Input placeholder="Pesquisar pelo nome" onChange={(e) => setName(e.target.value)}/>
+
+    <Button type="submit" variant="icon">
+        <img src={searchSvg} alt="Íconde de pesquisa" className="w-5" />
+    </Button>
 </form>
 
         </div>

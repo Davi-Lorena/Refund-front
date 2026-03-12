f6f2b665fe219c92e7827288b7ca9a33bebecde5
Davi Lorena <davilorena2016@gmail.com>
2026-03-12 12:22:01 -0300

    feat: adiciona integracao e logica para entrada no sistema com useactionstate

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,21 +1,49 @@
-import { useState } from "react"
+import { useActionState } from "react"
+import {z, ZodError} from "zod"
+import { api } from "../services/api"
+import { AxiosError } from "axios"
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
 
-export function SignIn() {
-const [email, setEmail] = useState("")
-const [password, setPassword] = useState("")
-const [isLoading, setIsLoading] = useState(false)
+const signInSchema = z.object({
+  email: z.string().email({message: "E-mail inválido"}),
+  password: z.string().trim().min(1, { message: "Informe a senha"})
+})
 
-function onSubmit(e: React.FormEvent) {
-  e.preventDefault()
-console.log(email, password)
+export function SignIn() {
+
+const [state, formAction, isLoading] = useActionState(signIn, null)
+
+async function signIn(_: any, FormData: FormData) {
+try {
+  const data = signInSchema.parse({
+  email: FormData.get("email"),
+  password: FormData.get("password")
+  })
+
+  const response = await api.post("/sessions", data)
+  
+} catch (error) {
+  console.log(error)
+  if(error instanceof ZodError){
+      return {message: error.issues[0].message}
+    }
+
+  if(error instanceof AxiosError){
+    return {message: error.response?.data.message}
+  }
+  return { message: "Não foi possível entrar!"}
+}
 }
   return (
-    <form onSubmit={onSubmit} className="flex flex-col gap-4 w-full">
-<Input required legend="E-mail" type="email" placeholder="seu@email" onChange={(e) => setEmail(e.target.value)}/>
+    <form action={formAction} className="flex flex-col gap-4 w-full">
+<Input name="email" required legend="E-mail" type="email" placeholder="seu@email" />
 
-<Input required legend="Senha" type="password" placeholder="123456" onChange={(e) => setPassword(e.target.value)}/>
+<Input name="password" required legend="Senha" type="password" placeholder="123456" />
+
+<p className="text-sm text-red-600 text-center my-4 cont-medium">
+  {state?.message}
+</p>
 
 <Button type="submit" isLoading={isLoading}>Entrar</Button>
 
